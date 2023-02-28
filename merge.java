06c12bab153d64f5796775090a280aa429c03443
Mayur Chaudhari <mayurchaudhari79@gmail.com>
2023-02-28 15:50:51 +0530

    second commit

--- merge.java
+++ merge.java
@@ -4,7 +4,7 @@ class Merge
     {
         int arr1[]= {1,2,3,4,5,6,7,8};
         int arr2[]={9,8,7,6,5,4,3,2,1};
-        int arr3[]=new int [gir ];
+        int arr3[]=new int [arr1.length+arr2.length ];
         int i=0;
         for(i=0;i<arr1.length;i++)
         {
