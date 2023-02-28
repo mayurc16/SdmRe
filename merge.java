2c16c243bf02b6df63f6737c1be99012959da263
Mayur Chaudhari <mayurchaudhari79@gmail.com>
2023-02-28 15:46:44 +0530

    first commit

---
+++ merge.java
@@ -0,0 +1,18 @@
+class Merge
+{
+    public static void main(String[] args)  
+    {
+        int arr1[]= {1,2,3,4,5,6,7,8};
+        int arr2[]={9,8,7,6,5,4,3,2,1};
+        int arr3[]=new int [gir ];
+        int i=0;
+        for(i=0;i<arr1.length;i++)
+        {
+            arr3[i]= arr1[i];
+        }
+        for(int j=0;j<arr2.length;i++,j++) 
+        {
+            arr3[i]= arr2[j];
+        }
+    }
+}
